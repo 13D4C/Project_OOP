f011a8a3888e19fc8bc49d085caf7078fff83091
Sirikul Khummung <135794016+Imparaloy@users.noreply.github.com>
2024-03-21 17:06:13 +0700

    add class User , Employee
    Class User , Employee Part OOP

---
+++ ooppart/Employee.java
@@ -0,0 +1,24 @@
+public class Employee {
+
+    private String name;
+    private int project_handel;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setProject_handle(int project_handel) {
+        this.project_handel = project_handel;
+    }
+    
+
+    public int getProject_handle() {
+        return project_handel;
+    }
+
+}
