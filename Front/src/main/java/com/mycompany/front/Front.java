ed2af9e0e62a95714b7829bff4c7d71232403b3e
D4C <115446911+13D4C@users.noreply.github.com>
2024-03-22 15:38:16 +0700

    Add files via upload

---
+++ Front/src/main/java/com/mycompany/front/Front.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.front;
+
+/**
+ *
+ * @author airew
+ */
+public class Front {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
