0cf28efb346bc2412fa716f652481db9aa1ebcf8
D4C <115446911+13D4C@users.noreply.github.com>
2024-03-23 20:33:28 +0700

    จัดให้สวย najaaa

---
+++ Component/SalesSummaryReportController.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package javafxapplication3;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+/**
+ * FXML Controller class
+ *
+ * @author oatsu
+ */
+public class SalesSummaryReportController implements Initializable {
+
+    @FXML
+    private Color x1;
+    @FXML
+    private Font x4;
+    @FXML
+    private Font x5;
+    @FXML
+    private Font x2;
+    @FXML
+    private Color x8;
+    @FXML
+    private Font x3;
+    @FXML
+    private Font x6;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
