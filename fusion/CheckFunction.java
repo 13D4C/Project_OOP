6ae9ded0a1e0f65275d217c11843f32656f32455
Sirikul Khummung <135794016+Imparaloy@users.noreply.github.com>
2024-04-01 01:03:21 +0700

    Custom Class by Parametric Polymorphism
    Custom Class โดยอาศัยหลักการ  Parametric Polymorphism

---
+++ fusion/CheckFunction.java
@@ -0,0 +1,17 @@
+public class CheckFunction<t> {
+    private t content;
+    
+    public void setContent(t content){
+        this.content = content;
+    }
+    
+    public t getContent(){
+        return content;
+    }
+    
+    public static void main(String[] args) {
+        CheckFunction<String> stringBox = new  CheckFunction<>();
+        stringBox.setContent("Complete");
+        System.out.println("Status Function: " + stringBox.getContent());
+    }
+}
