3f3c19a7c5de50f100a89191677542b17a1c5727
Sirikul Khummung <135794016+Imparaloy@users.noreply.github.com>
2024-04-01 20:33:44 +0700

    เรียกใช้ method  abstract class

--- fusion/Employee.java
+++ fusion/Employee.java
@@ -1,7 +1,12 @@
-package project;
-
 public class Employee extends User {
     public Employee(String username, String password, String id, String role) {
         super(username, password, id, role);
+    }
+    
+    @Override
+    public void displayUserInfo() {
+        System.out.println("Admin Username: " + getUsername());
+        System.out.println("Admin ID: " + getId());
+        System.out.println("Role: " + getRole());
     }
 }
