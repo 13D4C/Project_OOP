861b188822a784fdc97a20d1b53b04d8390d0911
Sirikul Khummung <135794016+Imparaloy@users.noreply.github.com>
2024-04-01 12:48:05 +0700

    add Method custom class

--- fusion/privacy.java
+++ fusion/privacy.java
@@ -1,8 +1,15 @@
 public class privacy {
+    
     public void Showable(Showable showable){
         
     }
-    public void User(User User){
+    
+    public void User(User user){
         
+    }
+    
+    public String Dateoption (Dateoption dateoption){
+        Dateoption a1 = new Dateoption();
+        return a1.getText();
     }
 }
