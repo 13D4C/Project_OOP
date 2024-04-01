cd5d88efb36066ba0c327315427371b4a3cd3ee2
Sirikul Khummung <135794016+Imparaloy@users.noreply.github.com>
2024-04-01 12:48:31 +0700

    Custom Class new

---
+++ fusion/Dateoption.java
@@ -0,0 +1,22 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Dateoption {
+    private String formattedDate;
+    private String formatted;
+    
+    public void setFunction() {
+        LocalDateTime myDateObj = LocalDateTime.now();
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("E, MMM dd yyyy HH:mm:ss");
+        formatted = myDateObj.format(myFormatObj);
+    }
+    
+    public void setText(){
+        formattedDate = formatted;
+    }
+   
+    public String getText(){
+        return this.formattedDate;
+    }
+
+}
